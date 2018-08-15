0dee0a9c168650215ebaf18492bebc9676f23cbd
Santiago Gomez Giraldo <santiago.gg@co-it01004.globant.com>
2018-08-15 13:20:10 -0500

    using Alamofire

--- TheMovieDB/TheMovieDB/ViewController.swift
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://api.themoviedb.org/3/genre/movie/list?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US").responseJSON { response in
+           /* print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")           */              // response serialization result
+            
+            if let json = response.result.value {
+               // print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                //print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+            
+            if let genreJSON = response.result.value{
+                let movieJSON:Dictionary = genreJSON as! Dictionary<String, Any>
+                let movieGenre = movieJSON["genres"] as! Array<Dictionary<String, Any>>
+                var iD:Int
+                var name:String
+                for i in 0..<movieGenre.count{
+                    iD = movieGenre[i]["id"] as! Int
+                    print(iD)
+                    name = movieGenre[i]["name"] as! String
+                    print(name)
+                }
+                
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
